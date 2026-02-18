bbbb55ccf3d542f3a901a1c55f3de2c28e938a3f
chetan jangid <jangidchetan.jpr@gmail.com>
2026-02-18 20:32:59 +0530

    Create Space_complexity.py

---
+++ Space_complexity.py
@@ -0,0 +1,3 @@
+space complexxity =total space taken by the program elements to solve and problaam and program to be executed is known as space complexity
+auxilary space =  space taken by the other varibale used to solve an problem
+input space =space taken by input variables 
