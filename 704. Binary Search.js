5d71276a6feaeade6347ecb33948b57e08a2331a
Chetan jangid <jangidchetan.jpr@gmail.com>
2026-04-12 19:55:00 +0530

    Create 704. Binary Search.js

---
+++ 704. Binary Search.js
@@ -0,0 +1,19 @@
+var search = function(nums, target) {
+    let low=0
+    let high=nums.length-1
+    let mid=0
+    while (low<=high){
+      mid=Math.floor((low+high)/2)
+      if(nums[mid]==target){
+        return mid // index
+      }
+      else if(nums[mid]<target){
+        low=mid+1;
+      }
+      else if (nums[mid]>target){
+        high=mid-1;
+    }
+    }
+    return -1
+  }
+    
